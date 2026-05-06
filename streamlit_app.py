4f37127163b69cd64bda7b045c57512c8918a16f
madsmilne <madelinemilne@kubrickgroup.com>
2026-05-06 16:03:52 +0100

    Update streamlit_app.py
    Pandas alteration

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customise Your Smoothie! :cup_with_straw:")
@@ -16,8 +17,13 @@ st.write('The name om your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop
+#Convert the Snowpark dataframe into a pandas dataframe so we can use th eLOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list = st.multiselect(
         'Choose up to 5 ingedients:'
         , my_dataframe
