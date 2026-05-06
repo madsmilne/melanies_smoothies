a34cec0e1bdcf0ca2c5bb0828f9c2130115153e8
madsmilne <madelinemilne@kubrickgroup.com>
2026-05-06 15:48:02 +0100

    Update streamlit_app.py
    fix multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
         'Choose up to 5 ingedients:'
         , my_dataframe
-        , max_selection = 5
+        , max_selections = 5
 )
 
 if ingredients_list: 
